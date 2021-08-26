e3724b3cc3059d6645417826ee2c93db33d7dee5
Himank Goel <himankgoel03@gmail.com>
2021-08-26 16:32:23 +0530

    Load data

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -8,3 +8,20 @@ def read_idx(filename):
         shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
         return np.frombuffer(f.read(), dtype=np.uint8).reshape(shape)
 
+x_train = read_idx("./fashion/train-images-idx3-ubyte")
+y_train = read_idx("./fashion/train-labels-idx1-ubyte")
+x_test = read_idx("./fashion/t10k-images-idx3-ubyte")
+y_test = read_idx("./fashion/t10k-labels-idx1-ubyte")
+
+print("Initial shape or dimensions of x_train", str(x_train.shape))
+
+print ("Number of samples in our training data: " + str(len(x_train)))
+print ("Number of labels in our training data: " + str(len(y_train)))
+print ("Number of samples in our test data: " + str(len(x_test)))
+print ("Number of labels in our test data: " + str(len(y_test)))
+print()
+print ("Dimensions of x_train:" + str(x_train[0].shape))
+print ("Labels in x_train:" + str(y_train.shape))
+print()
+print ("Dimensions of x_test:" + str(x_test[0].shape))
+print ("Labels in y_test:" + str(y_test.shape))
