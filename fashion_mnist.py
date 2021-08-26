59ecd31f8b58d03767b4d0848b1e3ba564632197
Himank Goel <himankgoel03@gmail.com>
2021-08-26 16:30:40 +0530

    First commit

---
+++ fashion_mnist.py
@@ -0,0 +1,10 @@
+import struct
+import numpy as np
+
+def read_idx(filename):
+    """Credit: https://gist.github.com/tylerneylon"""
+    with open(filename, 'rb') as f:
+        zero, data_type, dims = struct.unpack('>HBB', f.read(4))
+        shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
+        return np.frombuffer(f.read(), dtype=np.uint8).reshape(shape)
+
